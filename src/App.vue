2d2435b5536cb7f4821bec03b055d6e27660b3e4
qianzhikang <qzkqianzhikang@gmail.com>
2022-04-25 23:54:32 +0800

    附件店铺

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,6 @@
         class="wrapper__banner__img"
       />
     </div>
-
     <!-- 九宫格区域 -->
     <div class="wrapper__icons">
       <div class="wrapper__icons__item">
@@ -103,11 +102,108 @@
         <p class="wrapper__icons__item__desc">医药健康</p>
       </div>
     </div>
-
     <!-- 灰条分割 -->
     <div class="wrapper__gap"></div>
-  </div>
 
+    <!-- 附件店铺 -->
+    <div class="nearby">
+      <h3 class="nearby__title">附件店铺</h3>
+      <div class="nearby__item">
+        <img
+          src="https://pic-go.oss-cn-shanghai.aliyuncs.com/typora-img/202204252318920.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送0元</span>
+            <span class="nearby__content__tag">基础运费5元</span>
+          </div>
+          <div class="nearby__content__highlight">
+            VIP尊享满99减4元运费券（每月3张）
+          </div>
+        </div>
+      </div>
+
+      <div class="nearby__item">
+        <img
+          src="https://pic-go.oss-cn-shanghai.aliyuncs.com/typora-img/202204252344924.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送0元</span>
+            <span class="nearby__content__tag">基础运费5元</span>
+          </div>
+          <div class="nearby__content__highlight">
+            联合利华洗护满10减5
+          </div>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          src="https://pic-go.oss-cn-shanghai.aliyuncs.com/typora-img/202204252344924.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送0元</span>
+            <span class="nearby__content__tag">基础运费5元</span>
+          </div>
+          <div class="nearby__content__highlight">
+            联合利华洗护满10减5
+          </div>
+        </div>
+      </div>
+
+      <div class="nearby__item">
+        <img
+          src="https://pic-go.oss-cn-shanghai.aliyuncs.com/typora-img/202204252344924.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送0元</span>
+            <span class="nearby__content__tag">基础运费5元</span>
+          </div>
+          <div class="nearby__content__highlight">
+            联合利华洗护满10减5
+          </div>
+        </div>
+      </div>
+
+
+      <div class="nearby__item">
+        <img
+          src="https://pic-go.oss-cn-shanghai.aliyuncs.com/typora-img/202204252344924.png"
+          class="nearby__item__img"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">山姆会员店</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">起送0元</span>
+            <span class="nearby__content__tag">基础运费5元</span>
+          </div>
+          <div class="nearby__content__highlight">
+            联合利华洗护满10减5
+          </div>
+        </div>
+      </div>
+
+
+
+      
+
+    </div>
+  </div>
   <!-- 底部导航栏 -->
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -132,9 +228,9 @@
 <style lang="scss">
 @import "./style/index.scss";
 
-/*顶部 */ 
+/*顶部 */
 .wrapper {
-  overflow-y: hidden;
+  overflow-y: auto;
   position: absolute;
   left: 0;
   right: 0;
@@ -209,6 +305,50 @@
     height: 0.1rem;
     background-color: #f1f1f1;
   }
+
+  /* 附件店铺 */
+  .nearby {
+    &__title {
+      margin: 0.16rem 0 0.02rem 0;
+      font-size: 0.18rem;
+      font-weight: normal;
+      color: $content-fontcolor;
+    }
+    &__item {
+      display: flex;
+      padding-top: 0.12rem;
+      &__img{
+        margin-right: 0.16rem;
+        width: 0.56rem;
+        height: 0.56rem;
+      }
+    }
+    &__content{
+      flex: 1;
+      padding-bottom: 0.12rem;
+      border-bottom: 1px solid $border-color;
+      &__title {
+        line-height: 0.22rem;
+        font-size: 0.16rem;
+        color: $content-fontcolor;
+      }
+      &__tags {
+        margin-top: 0.08rem;
+        line-height: 0.18rem;
+        font-size: 0.13rem;
+        color: $content-fontcolor;
+      }
+      &__tag{
+        margin-right: 0.16rem;
+      }
+      &__highlight{
+        margin: 0.08rem 0 0 0;
+        line-height: 0.18rem;
+        font-size: 0.13rem;
+        color: #e93b3b;
+      }
+    }
+  }
 }
 
 /*底部导航栏 */
@@ -238,5 +378,4 @@
     transform-origin: center top;
   }
 }
-
 </style>
